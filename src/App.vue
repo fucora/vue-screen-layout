7c40d072199cbb260e85b1538ab8603527919c87
markyong <1654818917@qq.com>
2018-07-18 17:52:41 +0800

    init.......

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div style="margin: 17px">
+      宽：<input type="text" v-model.number="row" :key="1">
+      高：<input type="text" v-model.number="span" :key="2">
+    </div>
+    <vue-screen-layout :row="row" :span="span"></vue-screen-layout>
+  </div>
+</template>
+
+<script>
+import VueScreenLayout from './vue-screen-layout.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      row: 1,
+      span: 1
+    }
+  },
+  components: {
+    VueScreenLayout
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 1000px;
+  height: 500px;
+}
+</style>
---
+++ src/vue-screen-layout.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="core">
+    <div v-for="item in row" :key="item" :style="{width: rowWidth + '%', height: rowHeight + '%'}">
+      <div v-for="sub in span" :key="sub" :style="{width: spanWidth + '%', height: spanHeight + '%'}">
+        {{ sub }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'VueScreenLayout',
+  props: {
+    row: {
+      type: Number,
+      default: 1
+    },
+    span: {
+      type: Number,
+      default: 1
+    }
+  },
+  data () {
+    return {
+      rowWidth: 100,
+      spanHeight: 100
+    }
+  },
+  computed: {
+    rowHeight () {
+      return 100 / this.row
+    },
+    spanWidth () {
+      return 100 / this.span
+    }
+  }
+}
+</script>
+<style scoped>
+  .core {
+    width: 100%;
+    height: 100%;
+  }
+  div {
+    box-sizing: border-box;
+  }
+  .core div div {
+    border: 1px solid coral;
+    display: inline-block;
+  }
+</style>
